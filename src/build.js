5422d0835afee701ef2fa4c8c45b69d67fba434a
Prahlad Yeri <prahladyeri@yahoo.com>
2026-01-05 11:29:35 +0530

    chore: updated package.json, added images, ux updates

--- src/build.js
+++ src/build.js
@@ -1,3 +1,3 @@
 
   export const VERSION = "0.1.0";
-  export const BUILD = "2026-01-05T04:13:07.115Z";
+  export const BUILD = "2026-01-05T05:55:29.630Z";
--- src/controllers/about.js
+++ src/controllers/about.js
@@ -2,11 +2,23 @@ import { VERSION, BUILD } from '../build.js';
 
 export function index() 
 {
-	$("#app").html(`<div style='font-family: "cascadia code";'>
-	<h1>About Abhyasa</h1>
-	<pre>
-	Version: ${VERSION}
-	Build: ${BUILD}
+	$("#app").html(`<div style='font-size: 15px;'>
+	<pre >
+	<strong>ABOUT ABHYASA</strong>
+	
+	<strong>Version:</strong> ${VERSION}
+	<strong>Build:</strong> ${BUILD}
+	
+	<strong>Third party components used (Credits):<strong>
+	- <a href="https://jquery.com/" target="_blank" rel="noopener">jQuery</a> · <a href="https://getbootstrap.com/" target="_blank" rel="noopener">Bootstrap</a> · <a href="https://fontawesome.com/icons/" target="_blank" rel="noopener">Font awesome</a>
+	- <a href="https://www.cloudflare.com/" target="_blank" rel="noopener">Cloudflare Pages</a> (Hosting)
+	
+	<strong>Donate to help open source sustenance:<strong>
+	- <a class='text-bold' href="https://paypal.me/prahladyeri">Donate with PayPal</a>	
+	- <a href="upi://pay?pa=prahladyeri14-1@okaxis&cu=INR">Donate with UPI</a>
+	- Donate with UPI (scan QR code):
+	
+	<img src="./upi-axis.png" style='max-width:100px'>
 	</pre>
 	</div>
 	`);
